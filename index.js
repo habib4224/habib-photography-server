9d7bdcb6589e0357792d96a2044a55fa4ac82f19
habib4224 <4224habib1@gmail.com>
2022-11-10 18:59:11 +0600

    add servi

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
         });
 
 
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
