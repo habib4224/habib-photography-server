479fdd9c543639cb6fa2bdcd7b9393249cb7351f
habib4224 <4224habib1@gmail.com>
2022-11-10 18:56:41 +0600

    review

--- index.js
+++ index.js
@@ -72,6 +72,12 @@ async function run() {
             const result = await reviewService.updateOne(query, updatedDoc);
             res.send(result);
         })
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewService.deleteOne(query);
+            res.send(result);
+        })
 
 
     }
